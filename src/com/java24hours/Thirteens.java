58daac9dee3f742287e6d11fbaf6cfb9c6a7c6f4
Weirdddude <rlweirdddude@gmail.com>
2022-09-11 12:48:16 +0200

    Leanrt Loops

---
+++ src/com/java24hours/Thirteens.java
@@ -0,0 +1,10 @@
+package com.java24hours;
+
+public class Thirteens {
+    public static void main(String[] args) {
+        for(int dex = 1; dex <= 400; dex++){
+            int multiple = 13 * dex;
+            System.out.println(multiple);
+        }
+    }
+}
